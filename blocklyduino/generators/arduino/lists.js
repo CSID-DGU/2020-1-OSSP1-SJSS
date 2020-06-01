df5731514074b8f07160299a7fd587dcb5d3c3ff
ssuh0o0 <55631147+ssuh0o0@users.noreply.github.com>
2020-06-02 03:35:52 +0900

    Create lists.js

---
+++ blocklyduino/generators/arduino/lists.js
@@ -0,0 +1,280 @@
+/**
+ * @license
+ * Copyright 2012 Google LLC
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @fileoverview Generating Arduino for list blocks.
+ * @author q.neutron@gmail.com (Quynh Neutron)
+ */
+'use strict';
+
+goog.provide('Blockly.Arduino.lists');
+
+goog.require('Blockly.Arduino');
+
+
+Blockly.Arduino['lists_create_empty'] = function(block) {
+  // Create an empty list.
+  return ['[]', Blockly.Arduino.ORDER_ATOMIC];
+};
+
+Blockly.Arduino['lists_create_with'] = function(block) {
+  // Create a list with any number of elements of any type.
+  var elements = new Array(block.itemCount_);
+  for (var i = 0; i < block.itemCount_; i++) {
+    elements[i] = Blockly.Arduino.valueToCode(block, 'ADD' + i,
+        Blockly.Arduino.ORDER_NONE) || 'None';
+  }
+  var code = '[' + elements.join(', ') + ']';
+  return [code, Blockly.Arduino.ORDER_ATOMIC];
+};
+
+Blockly.Arduino['lists_repeat'] = function(block) {
+  // Create a list with one element repeated.
+  var item = Blockly.Arduino.valueToCode(block, 'ITEM',
+      Blockly.Arduino.ORDER_NONE) || 'None';
+  var times = Blockly.Arduino.valueToCode(block, 'NUM',
+      Blockly.Arduino.ORDER_MULTIPLICATIVE) || '0';
+  var code = '[' + item + '] * ' + times;
+  return [code, Blockly.Arduino.ORDER_MULTIPLICATIVE];
+};
+
+Blockly.Arduino['lists_length'] = function(block) {
+  // String or array length.
+  var list = Blockly.Arduino.valueToCode(block, 'VALUE',
+      Blockly.Arduino.ORDER_NONE) || '[]';
+  return ['len(' + list + ')', Blockly.Arduino.ORDER_FUNCTION_CALL];
+};
+
+Blockly.Arduino['lists_isEmpty'] = function(block) {
+  // Is the string null or array empty?
+  var list = Blockly.Arduino.valueToCode(block, 'VALUE',
+      Blockly.Arduino.ORDER_NONE) || '[]';
+  var code = 'not len(' + list + ')';
+  return [code, Blockly.Arduino.ORDER_LOGICAL_NOT];
+};
+
+
+Blockly.Arduino['lists_getIndex'] = function(block) {
+  // Get element at index.
+  // Note: Until January 2013 this block did not have MODE or WHERE inputs.
+  var mode = block.getFieldValue('MODE') || 'GET';
+  var where = block.getFieldValue('WHERE') || 'FROM_START';
+  var listOrder = (where == 'RANDOM') ? Blockly.Arduino.ORDER_NONE :
+      Blockly.Arduino.ORDER_MEMBER;
+  var list = Blockly.Arduino.valueToCode(block, 'VALUE', listOrder) || '[]';
+
+  switch (where) {
+    case 'FIRST':
+      if (mode == 'GET') {
+        var code = list + '[0]';
+        return [code, Blockly.Arduino.ORDER_MEMBER];
+      } else if (mode == 'GET_REMOVE') {
+        var code = list + '.pop(0)';
+        return [code, Blockly.Arduino.ORDER_FUNCTION_CALL];
+      } else if (mode == 'REMOVE') {
+        return list + '.pop(0)\n';
+      }
+      break;
+    case 'LAST':
+      if (mode == 'GET') {
+        var code = list + '[-1]';
+        return [code, Blockly.Arduino.ORDER_MEMBER];
+      } else if (mode == 'GET_REMOVE') {
+        var code = list + '.pop()';
+        return [code, Blockly.Arduino.ORDER_FUNCTION_CALL];
+      } else if (mode == 'REMOVE') {
+        return list + '.pop()\n';
+      }
+      break;
+    case 'FROM_START':
+      var at = Blockly.Arduino.getAdjustedInt(block, 'AT');
+      if (mode == 'GET') {
+        var code = list + '[' + at + ']';
+        return [code, Blockly.Arduino.ORDER_MEMBER];
+      } else if (mode == 'GET_REMOVE') {
+        var code = list + '.pop(' + at + ')';
+        return [code, Blockly.Arduino.ORDER_FUNCTION_CALL];
+      } else if (mode == 'REMOVE') {
+        return list + '.pop(' + at + ')\n';
+      }
+      break;
+    case'FROM_END':
+      var at = Blockly.Arduino.getAdjustedInt(block, 'AT', 1, true);
+      if (mode == 'GET') {
+        var code = list + '[' + at + ']';
+        return [code, Blockly.Arduino.ORDER_MEMBER];
+      } else if (mode == 'GET_REMOVE') {
+        var code = list + '.pop(' + at + ')';
+        return [code, Blockly.Arduino.ORDER_FUNCTION_CALL];
+      } else if (mode == 'REMOVE') {
+        return list + '.pop(' + at + ')\n';
+      }
+      break;
+    case 'RANDOM':
+      Blockly.Arduino.definitions_['import_random'] = 'import random';
+      if (mode == 'GET') {
+        code = 'random.choice(' + list + ')';
+        return [code, Blockly.Arduino.ORDER_FUNCTION_CALL];
+      } else {
+        var functionName = Blockly.Arduino.provideFunction_(
+            'lists_remove_random_item',
+            ['def ' + Blockly.Arduino.FUNCTION_NAME_PLACEHOLDER_ + '(myList):',
+              '  x = int(random.random() * len(myList))',
+              '  return myList.pop(x)']);
+        code = functionName + '(' + list + ')';
+        if (mode == 'GET_REMOVE') {
+          return [code, Blockly.Arduino.ORDER_FUNCTION_CALL];
+        } else if (mode == 'REMOVE') {
+          return code + '\n';
+        }
+      }
+      break;
+  }
+  throw Error('Unhandled combination (lists_getIndex).');
+};
+
+Blockly.Arduino['lists_setIndex'] = function(block) {
+  // Set element at index.
+  // Note: Until February 2013 this block did not have MODE or WHERE inputs.
+  var list = Blockly.Arduino.valueToCode(block, 'LIST',
+      Blockly.Arduino.ORDER_MEMBER) || '[]';
+  var mode = block.getFieldValue('MODE') || 'GET';
+  var where = block.getFieldValue('WHERE') || 'FROM_START';
+  var value = Blockly.Arduino.valueToCode(block, 'TO',
+      Blockly.Arduino.ORDER_NONE) || 'None';
+  // Cache non-trivial values to variables to prevent repeated look-ups.
+  // Closure, which accesses and modifies 'list'.
+  function cacheList() {
+    if (list.match(/^\w+$/)) {
+      return '';
+    }
+    var listVar = Blockly.Arduino.variableDB_.getDistinctName(
+        'tmp_list', Blockly.VARIABLE_CATEGORY_NAME);
+    var code = listVar + ' = ' + list + '\n';
+    list = listVar;
+    return code;
+  }
+
+  switch (where) {
+    case 'FIRST':
+      if (mode == 'SET') {
+        return list + '[0] = ' + value + '\n';
+      } else if (mode == 'INSERT') {
+        return list + '.insert(0, ' + value + ')\n';
+      }
+      break;
+    case 'LAST':
+        if (mode == 'SET') {
+          return list + '[-1] = ' + value + '\n';
+        } else if (mode == 'INSERT') {
+          return list + '.append(' + value + ')\n';
+        }
+      break;
+    case 'FROM_START':
+      var at = Blockly.Arduino.getAdjustedInt(block, 'AT');
+        if (mode == 'SET') {
+          return list + '[' + at + '] = ' + value + '\n';
+        } else if (mode == 'INSERT') {
+          return list + '.insert(' + at + ', ' + value + ')\n';
+        }
+      break;
+    case 'FROM_END':
+      var at = Blockly.Arduino.getAdjustedInt(block, 'AT', 1, true);
+        if (mode == 'SET') {
+          return list + '[' + at + '] = ' + value + '\n';
+        } else if (mode == 'INSERT') {
+          return list + '.insert(' + at + ', ' + value + ')\n';
+        }
+      break;
+    case 'RANDOM':
+        Blockly.Arduino.definitions_['import_random'] = 'import random';
+        var code = cacheList();
+        var xVar = Blockly.Arduino.variableDB_.getDistinctName(
+            'tmp_x', Blockly.VARIABLE_CATEGORY_NAME);
+        code += xVar + ' = int(random.random() * len(' + list + '))\n';
+        if (mode == 'SET') {
+          code += list + '[' + xVar + '] = ' + value + '\n';
+          return code;
+        } else if (mode == 'INSERT') {
+          code += list + '.insert(' + xVar + ', ' + value + ')\n';
+          return code;
+        }
+      break;
+  }
+  throw Error('Unhandled combination (lists_setIndex).');
+};
+
+Blockly.Arduino['lists_getSublist'] = function(block) {
+  // Get sublist.
+  var list = Blockly.Arduino.valueToCode(block, 'LIST',
+      Blockly.Arduino.ORDER_MEMBER) || '[]';
+  var where1 = block.getFieldValue('WHERE1');
+  var where2 = block.getFieldValue('WHERE2');
+  switch (where1) {
+    case 'FROM_START':
+      var at1 = Blockly.Arduino.getAdjustedInt(block, 'AT1');
+      if (at1 == '0') {
+        at1 = '';
+      }
+      break;
+    case 'FROM_END':
+      var at1 = Blockly.Arduino.getAdjustedInt(block, 'AT1', 1, true);
+      break;
+    case 'FIRST':
+      var at1 = '';
+      break;
+    default:
+      throw Error('Unhandled option (lists_getSublist)');
+  }
+  switch (where2) {
+    case 'FROM_START':
+      var at2 = Blockly.Arduino.getAdjustedInt(block, 'AT2', 1);
+      break;
+    case 'FROM_END':
+      var at2 = Blockly.Arduino.getAdjustedInt(block, 'AT2', 0, true);
+      // Ensure that if the result calculated is 0 that sub-sequence will
+      // include all elements as expected.
+      if (!Blockly.isNumber(String(at2))) {
+        Blockly.Arduino.definitions_['import_sys'] = 'import sys';
+        at2 += ' or sys.maxsize';
+      } else if (at2 == '0') {
+        at2 = '';
+      }
+      break;
+    case 'LAST':
+      var at2 = '';
+      break;
+    default:
+      throw Error('Unhandled option (lists_getSublist)');
+  }
+  var code = list + '[' + at1 + ' : ' + at2 + ']';
+  return [code, Blockly.Arduino.ORDER_MEMBER];
+};
+
+
+
+Blockly.Arduino['lists_split'] = function(block) {
+  // Block for splitting text into a list, or joining a list into text.
+  var mode = block.getFieldValue('MODE');
+  if (mode == 'SPLIT') {
+    var value_input = Blockly.Arduino.valueToCode(block, 'INPUT',
+        Blockly.Arduino.ORDER_MEMBER) || '\'\'';
+    var value_delim = Blockly.Arduino.valueToCode(block, 'DELIM',
+        Blockly.Arduino.ORDER_NONE);
+    var code = value_input + '.split(' + value_delim + ')';
+  } else if (mode == 'JOIN') {
+    var value_input = Blockly.Arduino.valueToCode(block, 'INPUT',
+        Blockly.Arduino.ORDER_NONE) || '[]';
+    var value_delim = Blockly.Arduino.valueToCode(block, 'DELIM',
+        Blockly.Arduino.ORDER_MEMBER) || '\'\'';
+    var code = value_delim + '.join(' + value_input + ')';
+  } else {
+    throw Error('Unknown mode: ' + mode);
+  }
+  return [code, Blockly.Arduino.ORDER_FUNCTION_CALL];
+};
+
+
